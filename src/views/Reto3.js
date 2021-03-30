1c81fc67a44b0b66d266593a7dc474224d00f28b
Mario Ricardo Dominguez Quiroz <darkmarthur@gmail.com>
2021-03-30 01:23:03 -0600

    FILE PURGE

--- src/views/Reto3.js
+++ src/views/Reto3.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Card, CardHeader, CardBody, CardTitle, Row, Col } from "reactstrap";
 
 import testNetworkData from "../data/VINC_PEF_2021.json";
-import graphStyle from "../data/ODS_style.js";
+import graphStyle from "../data/ODS_style.json";
 
 import Plot from "react-plotly.js";
 import Cytoscape from "cytoscape";
@@ -95,136 +95,7 @@ function Reto3() {
                       // autounselectify={true}
                       style={{ width: "100%", height: "1000px" }}
                       // stylesheet={graphStyle.style}
-                      stylesheet={[
-                        {
-                          selector: "node",
-                          css: {
-                            "border-width": 2.0,
-                            "border-color": "rgb(204,204,204)",
-                            color: "rgb(102,102,102)",
-                            "text-valign": "bottom",
-                            "text-halign": "right",
-                            width: 10.0,
-                            "background-opacity": 1.0,
-                            "font-family": "SansSerif.plain",
-                            "font-weight": "normal",
-                            "font-size": 10,
-                            "background-color": "rgb(254,196,79)",
-                            "text-opacity": 1.0,
-                            "border-opacity": 1.0,
-                            content: "",
-                            height: 10.0,
-                            shape: "ellipse",
-                          },
-                        },
-                        {
-                          selector: "node:selected",
-                          css: {
-                            "background-color": "rgb(255,255,0)",
-                          },
-                        },
-                        {
-                          selector: "edge",
-                          css: {
-                            "source-arrow-shape": "none",
-                            width: 0.5,
-                            color: "rgb(0,0,0)",
-                            "source-arrow-color": "rgb(204,204,204)",
-                            "target-arrow-shape": "triangle",
-                            "line-color": "rgb(204,204,204)",
-                            "text-opacity": 1.0,
-                            "target-arrow-color": "rgb(204,204,204)",
-                            "line-style": "solid",
-                            opacity: 1.0,
-                            "font-family": "SansSerif.plain",
-                            "font-weight": "normal",
-                            content: "",
-                            "font-size": 10,
-                          },
-                        },
-                        {
-                          selector: "edge[ id = '2594' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2564' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2533' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2534' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2598' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2441' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2445' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2578' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2547' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2516' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2580' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2517' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2456' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2488' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2489' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2553' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2617' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2586' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge[ id = '2526' ]",
-                          css: {},
-                        },
-                        {
-                          selector: "edge:selected",
-                          css: {
-                            "line-color": "rgb(255,0,0)",
-                          },
-                        },
-                      ]}
+                      stylesheet={graphStyle.style}
                       // style={graphStyle}
                     />
                   ) : null}
